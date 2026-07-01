63e11ba0dd6f8bfb14cfcf926fe47e0fffce0ac8
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-01 15:38:40 +0530

    Import Fiexed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router/router";
+import router from "./router/router";
 import "./index.css";
 import App from "./App.jsx";
 
