8250c8a8751dd1582717c1f6625d71a877e5bd7c
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-01 15:17:48 +0530

    MainLayout Done

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+const MainLayout = () => {
+  return (
+    <>
+      <h1>Navbar</h1>
+      <Outlet />
+      <h1>Footer</h1>
+    </>
+  );
+};
+
+export default MainLayout;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,22 +3,28 @@ import Home from "../components/Home";
 import About from "../components/About";
 import Login from "../components/Login";
 import Dashboard from "../components/Dashboard";
+import MainLayout from "../layouts/MainLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "about",
-    element: <About />,
-  },
-  {
-    path: "login",
-    element: <Login />,
-  },
-  {
-    path: "dashboard",
-    element: <Dashboard />,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+    ],
   },
 ]);
