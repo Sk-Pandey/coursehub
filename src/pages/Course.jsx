2040c2a537c1b4be828064348c906347fb86ca3c
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-02 15:54:35 +0530

    Using useParams for dynmic routing

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Course = () => {
+  const navigate = useNavigate();
   const courses = [
     { id: 1, name: "React" },
 
@@ -8,9 +10,20 @@ const Course = () => {
 
     { id: 3, name: "Node" },
   ];
-  return <div>
-    
-  </div>;
+  const handleNavigate = (id) => {
+    navigate(`/dashboard/courses/${id}`);
+  };
+  return (
+    <div>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <button onClick={() => handleNavigate(course.id)}>
+            {course.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Course;
