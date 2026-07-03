4b1dad6217ebe2d49e16f6cfa4599ae50271e0af
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-03 14:45:12 +0530

    Navbar convertedt to a component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,25 +1,10 @@
 import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 const MainLayout = () => {
-  const navLinkClasses = ({ isActive }) =>
-    `px-4 py-2 rounded-md transition-all duration-200 ${
-      isActive
-        ? "bg-blue-600 text-white"
-        : "text-gray-600 hover:bg-gray-100 hover:text-blue-600"
-    }`;
   return (
     <>
-      <nav className="flex justify-around py-4 font-bold text-2xl">
-        <NavLink className={navLinkClasses} to="/student" end>
-          Home
-        </NavLink>
-        <NavLink className={navLinkClasses} to="/student/about">
-          About
-        </NavLink>
-        <NavLink className={navLinkClasses} to="/student/login">
-          login
-        </NavLink>
-      </nav>
+      <Navbar />
       <div className="flex justify-center items-center h-150 text-5xl">
         <Outlet />
       </div>
