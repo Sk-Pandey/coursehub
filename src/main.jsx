faffc9cdf144ca4e2263b78bdbe042af8187f55d
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-04 15:10:31 +0530

    Context Created

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
 import "./index.css";
 import App from "./App.jsx";
+import AuthProvider from "./context/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 );
