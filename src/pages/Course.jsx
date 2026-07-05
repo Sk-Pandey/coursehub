306830d72df47bfaabdaf3d601790a68d3d9885b
Sk-Pandey <pandeyshashikant1434@gmail.com>
2026-07-05 06:00:52 +0530

    try catch error loading state handled

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -10,12 +10,12 @@ const Course = () => {
     const fetchCourse = async () => {
       try {
         const response = await fetch(
-          " https://jsonplaceholder.typicode.com/users",
+          "https://jsonplaceholder.typicode.com/users",
         );
         const data = await response.json();
         setCourses(data);
-      } catch {
-        setError("Somthing went wrong");
+      } catch (err) {
+        setError(err.message);
       } finally {
         setLoading(false);
       }
@@ -35,11 +35,22 @@ const Course = () => {
     return <h1>{error}</h1>;
   }
   return (
-    <div>
+    <div className="flex flex-wrap justify-around item-center gap-20">
       {courses.map((course) => (
-        <div key={course.id}>
-          <button onClick={() => handleNavigate(course.id)}>
+        <div
+          key={course.id}
+          className="flex justify-around items-center flex-col bg-slate-800 px-4 py-8 w-[300px] h-[300px] rounded-lg shadow-md shadow-white cursor-pointer hover:transform hover:-translate-y-1"
+        >
+          <h1 className="text-3xl text-rose-400 font-semibold font-serif">
             {course.name}
+          </h1>
+          <span className="text-gray-400 font-bold">Email: {course.email}</span>
+          <h2 className="text-xl text-teal-200">{course.company.name}</h2>
+          <button
+            onClick={() => handleNavigate(course.id)}
+            className="bg-white text-black rounded-lg py-2 px-3 font-semibold capitalize hover:bg-amber-200 cursor-pointer"
+          >
+            view Detail
           </button>
         </div>
       ))}
